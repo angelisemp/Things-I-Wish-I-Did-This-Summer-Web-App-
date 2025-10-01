a9ea057d222d8e32a713b9f7f1d4e0b4538c1f04
Angelise Perez <angelisep3@nycstudents.net>
2025-10-01 12:47:20 -0400

    change2

---
+++ script.js
@@ -0,0 +1,12 @@
+let activities = [
+{ place: "the beach", activity: "swimming" },
+{ place: "city", activity: "shopping" },
+{ place: "Puerto Rico", activity: "traveling" },
+{ place: "a concert", activity: "singing" },
+{ place: "summer house", activity: "sleep" }
+];
+
+function generateWish() {
+
+}
+
